76938de4008747b44f7b6a7dfae27c8fd2288a69
Iman Homayouni <e2ma3n@gmail.com>
2019-06-12 03:58:46 +0430

    Update send-email.sh

--- send-email.sh
+++ send-email.sh
@@ -23,14 +23,14 @@ config_file='/opt/send-email/send-email.conf'
 # comment : check config file
 if [ ! -f "$config_file" ] ; then
     echo -e "\e[91m[!] error. /opt/send-email/send-email.conf not found. exit.\e[0m"
-    exit
+    exit 1
 fi
 
 
 # comment : check heirloom-mailx
 if [ -z `which heirloom-mailx 2> /dev/null` ] ; then
     echo -e "\e[91m[!] error. heirloom-mailx not found. exit.\e[0m"
-    exit
+    exit 1
 fi
 
 
@@ -46,7 +46,7 @@ smtp_user=$(head -n 11 "$config_file" | tail -n 1 | cut -d = -f 2)
 smtp_pass=$(head -n 12 "$config_file" | tail -n 1 | cut -d = -f 2)
 
 
-# comment : read data from argoment
+# comment : read data from argument
 send_to=$(echo "$3")
 title=$(echo "$2")
 
@@ -56,11 +56,12 @@ if [ -f "$1" &> /dev/null ] ; then
     time=`date +'%e %b %Y - %H:%M:%S'`
     sed "1 i\\$time\nMassege:" "$1" > /tmp/send-mail.txt
     cat /tmp/send-mail.txt | heirloom-mailx -v -r "$smtp_user" -s "$title" -S smtp=$smtp_srv -S smtp-use-starttls -S smtp-auth=login -S smtp-auth-user=$smtp_user -S smtp-auth-password=$smtp_pass -S ssl-verify=ignore -S nss-config-dir=/etc/pki/nssdb/ $send_to
-    rm -rf /tmp/send-mail.txt
+    rm -rf /tmp/send-mail.txt &> /dev/null
     exit 0
 else
     text_message=$(echo "$1")
     time=`date +'%e %b %Y - %H:%M:%S'`
     text_message=`echo "Date and time: $time" ; echo "Massege: $text_message"`
     echo "$text_message" | heirloom-mailx -v -r "$smtp_user" -s "$title" -S smtp=$smtp_srv -S smtp-use-starttls -S smtp-auth=login -S smtp-auth-user=$smtp_user -S smtp-auth-password=$smtp_pass -S ssl-verify=ignore -S nss-config-dir=/etc/pki/nssdb/ $send_to
+    exit 0
 fi
